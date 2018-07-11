65fdf48056db1684be097774c106d19c327ccbc0
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2018-07-11 14:41:16 +0530

    all section in index.html has been divided in different component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './adminlte/main-header/main-header.component';
+import { MainSidebarComponent } from './adminlte/main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './adminlte/content-wrapper/content-wrapper.component';
+import { MainFooterComponent } from './adminlte/main-footer/main-footer.component';
+import { ControlSidebarComponent } from './adminlte/control-sidebar/control-sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    MainFooterComponent,
+    ControlSidebarComponent
   ],
   imports: [
     BrowserModule
