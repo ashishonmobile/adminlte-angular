2eafe5c7d1f66f8a669215622828e64e8803d0a1
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2018-07-11 18:48:36 +0530

    I do not have any option right now without using jquery and bootstrap js. may be I will fix it later.

---
+++ src/app/adminlte/app-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModel} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Dashboard1Component} from './pages/dashboard1/dashboard1.component';
+import {Dashboard2Component} from './pages/dashboard2/dashboard2.component';
+
+const appRoute: Routes = [
+  {path: '', component: Dashboard1Component},
+  {path: 'dashboard1', component: Dashboard1Component},
+  {path: 'dashboard2', component: Dashboard2Component}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoute)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ControlSidebarComponent } from './adminlte/control-sidebar/control-side
 import {DropdownDirective} from './adminlte/shared/dropdown.directive';
 import { DemoDropdownBasicComponent } from './adminlte/demo-dropdown-basic/demo-dropdown-basic.component';
 import {BsDropdownModule} from 'ngx-bootstrap';
+import { Dashboard1Component } from './adminlte/pages/dashboard1/dashboard1.component';
+import {AppRoutingModule} from './adminlte/app-routing.module';
+import { Dashboard2Component } from './adminlte/pages/dashboard2/dashboard2.component';
 
 
 @NgModule({
@@ -21,11 +24,14 @@ import {BsDropdownModule} from 'ngx-bootstrap';
     MainFooterComponent,
     ControlSidebarComponent,
     DropdownDirective,
-    DemoDropdownBasicComponent
+    DemoDropdownBasicComponent,
+    Dashboard1Component,
+    Dashboard2Component
   ],
   imports: [
     BrowserModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
